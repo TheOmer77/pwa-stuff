ed20d4d711c0cf81ff63323698795e28ddfa001e
Omer <27678413+TheOmer77@users.noreply.github.com>
2021-03-02 10:17:59 +0200

    Add a react PWA

---
+++ react-pwa/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  console.log('i am log - edited!!');
+  return (
+    <div className='App'>
+      <h1>Hello world!!!</h1> Hello hello hello hello hello hello
+      <img src='testImg.png' alt='Test'></img>
+    </div>
+  );
+};
+
+export default App;
